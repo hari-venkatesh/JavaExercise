f896b6b67a8f48aa27e1a1b4a56343c56120e439
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-08-26 21:29:27 +0530

    Code Snippets

---
+++ practice/workout/Adder.java
@@ -0,0 +1,14 @@
+package workout;
+
+
+public class Adder {
+
+	public static void main(String[] values) {
+		float sum = 0;
+		for(int i=0;i<values.length;i++)
+		{
+			sum=sum+Float.parseFloat(values[i]);
+		}
+		System.out.printf("%.2f",sum);
+	}
+}
