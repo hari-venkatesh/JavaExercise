7c3333b588c441b2daff269a3212db85db13887e
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-08-29 16:27:04 +0530

    Sub Class Of Class Animal

---
+++ practice/inheritancedemo/Snake.java
@@ -0,0 +1,14 @@
+package inheritancedemo;
+
+public class Snake extends Animal {
+	String name;
+	int size;
+	
+	public void A(String type,String color,int size)
+	{
+		this.type=type;
+		this.color=color;
+		this.size=size;
+		System.out.println("s2");
+	}
+}
