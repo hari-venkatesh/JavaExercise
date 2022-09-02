9f5e383b51ffd02e0fadc8501e185832bf9804b2
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-09-02 11:52:27 +0530

    Small Changes

--- practice/workout/Fibonacci.java
+++ practice/workout/Fibonacci.java
@@ -3,13 +3,13 @@ package workout;
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		int sum = 0;
-		int first = 0;
-		int second = 1;
+		long sum = 0;
+		long first = 0;
+		long second = 1;
 		System.out.print(first+",");
 		System.out.print(second);
 		int i = 0;
-		while( i<10)
+		while( i<100)
 		{
 			sum=first+second;
 			System.out.print(","+sum);
